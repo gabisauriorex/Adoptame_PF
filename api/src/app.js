fc5da38b0d6a83df39f487c8432378bdf9284e84
Gabriel Romero <93299000+gabisauriorex@users.noreply.github.com>
2023-03-16 10:59:58 -0300

    "cambio server/app"

--- api/src/app.js
+++ api/src/app.js
@@ -36,14 +36,14 @@ app.use((req, res, next) => {
   next();
 });
 
-server.use('/', routes);
+app.use('/', routes);
 
 // Error catching endware.
-server.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
   const status = err.status || 500;
   const message = err.message || err;
   console.error(err);
   res.status(status).send(message);
 });
 
-module.exports = server;
+module.exports = app;
