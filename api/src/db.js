0f420b8768458be7c922029ddf84aec3d4fdb65d
MyriamGG <my_r_i@hotmail.com>
2023-03-16 10:14:39 -0300

    ultimos cambios de sequelize

--- api/src/db.js
+++ api/src/db.js
@@ -1,20 +1,30 @@
 require('dotenv').config();
 const {Sequelize} = require("sequelize");
-const database = require("./config");
 const path = require("path");
 const fs = require("fs");
 
-// const url = "mongodb+srv://admin:4Uu2hniHQ0UnmpWd@cluster0.5visjvr.mongodb.net/test"
-// //const URL = "mongodb+srv://<nombre usuario>:password@cluster0.bsvp1kl.mongodb.net/Adoptame?retryWrites=true&w=majority";
-// // "mongodb+srv://arieltecnico:1NOcCreyIcc5UnvH@cluster0.bsvp1kl.mongodb.net/Adoptame";
+const { DB_USER, DB_PASSWORD, DB_HOST, DB_NAME } = process.env;
 
-// const cnn = async () => {
-//   await mongoose.connect(url, {
-//     keepAlive: true,
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//   });
-// }
+const sequelize = new Sequelize(
+  `postgres://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}`,
+  {
+    logging: false, // set to console.log to see the raw SQL queries
+    native: false, // lets Sequelize know we can use pg-native for ~30% more speed
+  }
+);
+const basename = path.basename(__filename);
+
+const modelDefiners = [];
+
+// Leemos todos los archivos de la carpeta Models, los requerimos y agregamos al arreglo modelDefiners
+fs.readdirSync(path.join(__dirname, "/models"))
+  .filter(
+    (file) =>
+      file.indexOf(".") !== 0 && file !== basename && file.slice(-3) === ".js"
+  )
+  .forEach((file) => {
+    modelDefiners.push(require(path.join(__dirname, "/models", file)));
+  });
 // Injectamos la conexion (sequelize) a todos los modelos
 modelDefiners.forEach(model => model(sequelize));
 // Capitalizamos los nombres de los modelos ie: product => Product
