df25f7a77a03b2a8b216a5135e7fde65040d1bd4
MyriamGG <my_r_i@hotmail.com>
2023-03-21 21:30:40 -0300

    (feat(modelo: Usuario, Usuario-routes, Usuario-controller, db): Agregado de Usuario

---
+++ api/src/routes/Usuario-routes.js
@@ -0,0 +1,18 @@
+const express = require("express");
+const router = express.Router();
+const {
+  createUsuario,
+  getUsuario,
+  UsuarioById,
+  deleteUsuario,
+  updateUsuario,
+} = require("../controllers-Usuario/Usuario-controller");
+
+
+router.get("/", getUsuario); //trae todos los usuarios
+router.get("/:id", UsuarioById); //trae por id
+router.post("/", createUsuario); //crea una usuario
+router.put("/:id", updateUsuario); //actualiza un usuario
+router.delete("/:id", deleteUsuario); //elimina un usuario
+
+module.exports = router;
