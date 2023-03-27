6a29139b0a224aaf27809e50c63a4186249e8375
MyriamGG <my_r_i@hotmail.com>
2023-03-27 09:56:15 -0300

    agregado de Auth0 en el proyecto

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,19 +1,13 @@
 const { Router } = require("express");
 const router = Router();
 
+
 const Pet = require("./Mascota-routes");
 const Vaccines = require("./Vaccines-routes");
 const Diseases = require("./Diseases-routes");
 const Location = require("./Location-routes");
 const Usuario = require("./Usuario-routes");
 const Payment = require("./Payment-routes")
-
-const usersRouter = require('./user-routes')
-const loginRouter = require('./login-routes')
-
-router.use('/api/users', usersRouter)
-router.use('/api/login', loginRouter)
-
 
 
 router.use("/pets", Pet);
@@ -24,3 +18,4 @@ router.use("/users", Usuario);
 router.use("/payments",Payment)
 
 module.exports = router;
+
--- api/src/routes/login-routes.js
+++
@@ -1,8 +0,0 @@
-
-const loginRouter = require('express').Router()
-
-const Post_Login = require('../controllers-Auth0/Post_Login');
-
-loginRouter.post('/', Post_Login);
-
-module.exports = loginRouter
