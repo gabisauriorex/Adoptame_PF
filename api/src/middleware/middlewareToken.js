69bfdd59ed3528d1009512ecf215896d1f021f08
arielZarate <arieltecnico@gmail.com>
2023-03-29 17:42:38 -0300

    auth0 verifyJwt y middleware

---
+++ api/src/middleware/middlewareToken.js
@@ -0,0 +1,25 @@
+const axios = require('axios');
+const authConfig  = require('../auth_config.json');
+
+const middlewareToken = async (req, res, next) => { 
+
+  try{
+        console.log('aca tomo el token ')
+        const accessToken = req.headers.authorization.split(" ")[1];
+      
+        const response = await axios(`https://${authConfig.domain}/userinfo`, {
+          headers: {
+            authorization: `Bearer ${accessToken}`
+          }
+        })  
+
+        console.log('Datos del middleware: \n', response.data)
+  
+      }catch(error) {
+         res.send(error.message);
+       }
+      
+       next();
+    }
+
+    module.exports = middlewareToken;
--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -2,15 +2,14 @@ const { Router } = require("express");
 const router = Router();
 
 const Autenticacion = require("../middleware/Autenticacion");
-const userAutenticated = require("../middleware/userAutenticated");
-const autenticados = require("../routes/autenticados-routes");
+const middlewareToken = require("../middleware/middlewareToken");
+const allRoutes = require("./allRoutes");
 
   //===================
- const checkJwt = Autenticacion();
- router.use(checkJwt);
- checkJwt.unless({ path:[ "/" ] });
-
-router.use("/home", userAutenticated, autenticados);
+ //const checkJwt = Autenticacion();
+ //router.use(checkJwt);
+ //checkJwt.unless({ path:[ "/" ] })
+router.use("/api",allRoutes);
 
 module.exports = router;
 
